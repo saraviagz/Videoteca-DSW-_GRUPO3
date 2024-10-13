c3d4dbe5a2d4e70fda5c015c73beefbfa26bce94
aj <aj@PC-ELITE>
2024-10-12 20:13:46 -0500

    Agregar archivos de proyecto.

---
+++ Videoteca DSW _GRUPO3/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Videoteca_DSW__GRUPO3.Models;
+
+namespace Videoteca_DSW__GRUPO3.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Videoteca DSW _GRUPO3/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Videoteca_DSW__GRUPO3.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
