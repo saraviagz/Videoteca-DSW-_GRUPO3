e811f3409f623e8857daf1c7f493b9010dfe5247
SARAVIA RIOS <149112235+saraviagz@users.noreply.github.com>
2024-10-15 14:42:44 -0500

    PROBANDO2 LOGIN 15/10/24

---
+++ Videoteca DSW _GRUPO3/Models/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore; // Asegúrate de tener esta referencia
+using Videoteca_DSW__GRUPO3.Models;
+
+namespace Videoteca_DSW__GRUPO3.Models
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Administrador> Administradores { get; set; } // Refleja la tabla dbo.Administrador
+        public DbSet<Estudiante > Estudiantes { get; set; } // Refleja la tabla dbo.Administrador
+    }
+}
--- Videoteca DSW _GRUPO3/Program.cs
+++ Videoteca DSW _GRUPO3/Program.cs
@@ -1,7 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+using Videoteca_DSW__GRUPO3.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+// Configura la cadena de conexión a la base de datos con Entity Framework Core
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
